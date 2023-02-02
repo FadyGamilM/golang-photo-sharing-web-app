8802bd71818ae112b2c33ac9543c5c9e30e17cca
fady <gamilfady605@gmail.com>
2023-02-02 16:44:57 +0200

    adding contact page handler

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/FadyGamilM/photosharing
+
+go 1.19
--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 	"net/http"
 )
 
-func handlerfunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Photo Sharing App</h1>")
+func homeHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get request is done")
+	fmt.Fprintf(w, "<h1>Home Page </h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+}
+
+func contactHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+      <h1>Contact Page</h1>
+      <p>For more info, you can contact with me at <a href=/"gamilfady605@gmail.com/"> gamilfady605@gmail.com </a> </p>
+   `)
 }
 
 func main() {
-	http.HandleFunc("/", handlerfunc)
+	http.HandleFunc("/", homeHandlerFunc)
+	http.HandleFunc("/contact", contactHandlerFunc)
 	fmt.Println("server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
- 
