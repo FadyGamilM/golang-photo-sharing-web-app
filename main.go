ef842771a57b04d0a2c23d260ec6bd5d1b0a8153
fady <gamilfady605@gmail.com>
2023-02-03 08:29:37 +0200

    adding one more FAQ page handler

--- main.go
+++ main.go
@@ -6,35 +6,31 @@ import (
 )
 
 func homeHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get request is done")
-	fmt.Fprintf(w, "<h1>Home Page </h1>")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "<h1>Home Page </h1>")
 }
 
 func contactHandlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, `
       <h1>Contact Page</h1>
       <p>For more info, you can contact with me at <a href=/"gamilfady605@gmail.com/"> gamilfady605@gmail.com </a> </p>
    `)
 }
 
-func pathRouterHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandlerFunc(w, r)
-		return
-
-	case "/contact":
-		contactHandlerFunc(w, r)
-		return
-
-	default:
-		// w.WriteHeader(http.StatusNotFound)
-		// fmt.Fprint(w, "<h1> Page Not Found ! </h1>")
-		http.Error(w, "page not found", http.StatusNotFound)
-		return
-	}
+func FAQHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w,
+		`
+		<h1> FAQ Page </h1>
+		<h3> Can i have a free trial ? </h3>
+			<p> Yes, we have a free trial for 30 days with money back guranteed </p>
+		<h3> Do you have a supportl ? </h3>
+			<p> Yes, you can contact us at support@photoSharing.com we have a support team answering your questions 24/7 </p>
+		`)
 }
 
 // our custom router which implementes the `Handler` interface
@@ -51,6 +47,10 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		contactHandlerFunc(w, r)
 		return
 
+	case "/FAQ":
+		FAQHandlerFunc(w, r)
+		return
+
 	default:
 		// w.WriteHeader(http.StatusNotFound)
 		// fmt.Fprint(w, "<h1> Page Not Found ! </h1>")
@@ -62,9 +62,13 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// instantiate our custom router
 	router := Router{}
+
 	// http.HandleFunc("/", homeHandlerFunc)
 	// http.HandleFunc("/contact", contactHandlerFunc)
 	// http.HandleFunc("/", pathRouterHandler)
+
 	fmt.Println("server is running on port 3000")
+
+	// http.ListenAndServe("127.0.0.1:3000", http.HandlerFunc(pathRouterHandler))
 	http.ListenAndServe("127.0.0.1:3000", router)
 }
